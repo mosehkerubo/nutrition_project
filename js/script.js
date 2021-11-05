1be383fa7204db00ef534341d608863931582184
mosehkerubo <mosehkerubo@gmail.com>
2021-11-05 11:14:18 +0300

    changes

---
+++ js/script.js
@@ -0,0 +1,43 @@
+const myInputs=document.querySelector(".input");
+
+const myParagraph=document.getElementById("paragraph")
+const myEmpty=document.getElementById("empty-div")
+const myLogin=document.querySelector(".login-header")
+
+const mySubmit=document.querySelector("#submit");
+const myName=document.getElementById("name")
+
+mySubmit.addEventListener("click",(e)=>{
+    e.preventDefault();
+myLogin.style.display="none";
+document.write("After submitting form we will contact you shortly")
+
+
+
+
+
+
+// myParagraph.textContent="After submitting form we will contact you shortly"
+
+
+// mySubmit.appendChild(myParagraph)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(myParagraph)
+
+})
+
